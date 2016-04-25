f12aef431fe8cd11d56d2f131bcfa5df955a9550
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-04-25 13:08:25 +0200

    af_ktls: separated dtls window handling
    This allows for easier unit testing of the functionality.

--- af_ktls.c
+++ af_ktls.c
@@ -101,30 +101,6 @@
 						(T)->iv_recv && \
 						KTLS_GETSOCKOPT_READY(T))
 
-/*
- * DTLS sliding window handling
- */
-#define DTLS_EPOCH_SHIFT		(6*CHAR_BIT)
-#define DTLS_SEQ_NUM_MASK		0x0000FFFFFFFFFFFF
-
-#define DTLS_WINDOW_INIT(W)		((W).bits = (W.start) = 0)
-
-#define DTLS_SAME_EPOCH(S1, S2)		(((S1) >> DTLS_EPOCH_SHIFT) == ((S2) >> DTLS_EPOCH_SHIFT))
-
-#define DTLS_WINDOW_INSIDE(W, S)	((((S) & DTLS_SEQ_NUM_MASK) > (W).start) && \
-						(((S)  & DTLS_SEQ_NUM_MASK) - (W).start <= (sizeof((W).bits) * CHAR_BIT)))
-
-#define DTLS_WINDOW_OFFSET(W, S)	((((S) & DTLS_SEQ_NUM_MASK) - (W).start) - 1)
-
-#define DTLS_WINDOW_RECEIVED(W, S)	(((W).bits & ((u64) 1 << DTLS_WINDOW_OFFSET(W, S))) != 0)
-
-#define DTLS_WINDOW_MARK(W, S)		((W).bits |= ((u64) 1 << DTLS_WINDOW_OFFSET(W, S)))
-
-#define DTLS_WINDOW_UPDATE(W)		while ((W).bits & (u64) 1) { \
-						(W).bits = (W).bits >> 1; \
-						(W).start++; \
-					}
-
 #define IS_TLS(T)			(T->tls)
 #define IS_DTLS(T)			(!IS_TLS(T))
 
@@ -388,38 +364,7 @@ static void tls_update_senpage_ctx(struct tls_sock *tsk, size_t size)
 	sg_mark_end(&sg[tsk->sendpage_ctx.used]);
 }
 
-/*
- * Handle DTLS sliding window
- * rv: rv < 0  drop packet
- *     rv == 0 OK
- */
-static inline int dtls_window(struct tls_sock *tsk, const char * sn)
-{
-	uint64_t *seq_num_ptr, *seq_num_last_ptr;
-	uint64_t seq_num, seq_num_last;
-
-	seq_num_ptr = (uint64_t *) sn;
-	seq_num_last_ptr = (uint64_t *) tsk->iv_recv;
-	
-	seq_num = be64_to_cpu(*seq_num_ptr);
-	seq_num_last = be64_to_cpu(*seq_num_last_ptr);
-
-	if (!DTLS_SAME_EPOCH(seq_num_last, seq_num))
-		return -1;
-
-	/* are we inside sliding window? */
-	if (!DTLS_WINDOW_INSIDE(tsk->dtls_window, seq_num))
-		return -2;
-
-	/* already received? */
-	if (DTLS_WINDOW_RECEIVED(tsk->dtls_window, seq_num))
-		return -3;
-
-	DTLS_WINDOW_MARK(tsk->dtls_window, seq_num);
-	DTLS_WINDOW_UPDATE(tsk->dtls_window);
-
-	return 0;
-}
+#include "dtls-window.c"
 
 static void tls_data_ready(struct sock *sk)
 {
